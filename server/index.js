74a3b1c9221947dc71d577bbfa95317f12a40441
AdityyaVermma <adityaverma.asn02@gmail.com>
2026-02-23 22:55:42 +0530

    edit cors

--- server/index.js
+++ server/index.js
@@ -32,7 +32,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : "smart-health-pi.vercel.app"
+}));
 app.use(express.json());
 
 // Root Route
